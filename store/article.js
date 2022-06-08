821d72dd72a1a478372c69180919e27f991986af
ZYO-BAIKER <keisukeminakawa@gmail.com>
2022-06-09 03:15:21 +0900

    API を叩いて取得した data を store ファイルの state に保存
    state と mutation を定義し、レスポンスの記事一覧の data を state に保存

--- store/article.js
+++ store/article.js
@@ -1,6 +1,21 @@
+export const state = () => {
+  return {
+    articles: [],
+  }
+}
+
+export const getters = {}
+
+export const mutations = {
+  setArticles(state, articles) {
+    state.articles = articles
+  },
+}
+
 export const actions = {
   async fetchArticles({ commit }) {
     const response = await this.$axios.get('api/v1/articles')
-    console.log(response.data)
+    const articles = response.data
+    commit('setArticles', articles)
   },
 }
