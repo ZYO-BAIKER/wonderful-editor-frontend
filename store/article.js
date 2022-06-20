26aeaff4914b550c098c08967d5b5faa7c5cdc9e
ZYO-BAIKER <keisukeminakawa@gmail.com>
2022-06-21 08:50:08 +0900

    記事詳細のリンクを取得

--- store/article.js
+++ store/article.js
@@ -1,16 +1,22 @@
 export const state = () => {
   return {
     articles: [],
+    article: {},
   }
 }
 
 export const getters = {
   articles: (state) => state.articles,
+  article: (state) => state.article,
 }
 
 export const mutations = {
   setArticles(state, articles) {
     state.articles = articles
+  },
+
+  setArticle(state, article) {
+    state.article = article
   },
 }
 
@@ -23,5 +29,11 @@ export const actions = {
 
   async createArticle(_, paramas) {
     await this.$axios.post('api/v1/articles', paramas)
+  },
+
+  async fetchArticle({ commit }, id) {
+    const response = await this.$axios.get(`api/v1/articles/${id}`)
+    const article = response.data
+    commit('setArticle', article)
   },
 }
