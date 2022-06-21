fa2455161de213ad4f97cafda3e85c131e58752f
ZYO-BAIKER <keisukeminakawa@gmail.com>
2022-06-21 10:57:26 +0900

    記事詳細ページに削除ボタンを設置

--- pages/articles/_id/index.vue
+++ pages/articles/_id/index.vue
@@ -2,14 +2,18 @@
   <v-container class="elevation-3" :class="$style.article_container">
     <template v-if="isInitialized">
       <div :class="$style.article_layout">
-        <div :class="$style.name_area">
-          <span>@{{ article.user.name }}</span>
+        <v-layout :class="$style.name_area">
+          <span :class="$style.user_name">@{{ article.user.name }}</span>
           <timeago
             :class="$style.time_ago"
             :datetime="article.updated_at"
             :auto-update="60"
           />
-        </div>
+          <v-spacer></v-spacer>
+          <v-btn text fab small>
+            <v-icon color="#3085DE">fas fa-trash-alt</v-icon>
+          </v-btn>
+        </v-layout>
         <h1 :class="$style.article_title">{{ article.title }}</h1>
         <div :class="$style.article_body_container">
           <div :class="$style.article_body">
