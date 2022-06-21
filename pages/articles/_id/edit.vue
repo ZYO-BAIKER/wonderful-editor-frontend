59e8eab4d31656d452654d416c7833011e558989
ZYO-BAIKER <keisukeminakawa@gmail.com>
2022-06-21 17:42:54 +0900

    更新機能の実装
    保存されている値がフォームに表示

--- pages/articles/_id/edit.vue
+++ pages/articles/_id/edit.vue
@@ -21,7 +21,10 @@
       />
     </div>
     <div :class="$style.create_btn_area">
-      <v-btn color="#3085DE" class="font-weight-bold white--text"
+      <v-btn
+        color="#3085DE"
+        class="font-weight-bold white--text"
+        @click="updateArticle(article.id)"
         >記事を更新
       </v-btn>
     </div>
@@ -42,6 +45,50 @@ export default {
 
       loading: false,
     }
+  },
+
+  // created の時点でメソッドを発火させて記事の情報を取得
+  async created() {
+    await this.fetchArticle()
+  },
+
+  methods: {
+    async fetchArticle() {
+      const articleId = this.$route.params.id
+
+      try {
+        await this.$store.dispatch('article/fetchArticle', articleId)
+        const article = this.$store.getters['article/article']
+
+        // 用意しておいた data に取得した値を代入
+        this.article.id = article.id
+        this.article.title = article.title
+        this.article.body = article.body
+      } catch (err) {
+        // 暫定的な Error 表示
+        alert(err.response.data.errors.full_messages)
+      }
+    },
+
+    async updateArticle(id) {
+      // 多重送信防止
+      this.loading = true
+      const params = {
+        title: this.article.title,
+        body: this.article.body,
+      }
+      try {
+        // 複数の値を指定する場合はオブジェクトで指定する必要がある。
+        await this.$store.dispatch('article/updateArticle', { id, params })
+        // 記事詳細ページへ
+        this.$router.push(`/articles/${id}`)
+      } catch (err) {
+        // 暫定的な Error 表示
+        alert(err.response.statusText)
+      } finally {
+        this.loading = false
+      }
+    },
   },
 }
 </script>
